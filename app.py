354999a73fef73e8b93549e31e929a5c561ffa36
Nibs007 <83820201+Nibs007@users.noreply.github.com>
2021-05-09 16:32:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ body = html.Div([
         dbc.Row([
             dbc.Col([dbc.Row([dbc.Col(html.Div([
     html.Img(src='data:image/jpg;base64,{}'.format(encoded_image.decode()), 
-             style={'height': '300px','width': '500px',"margin-left": "2px","margin-right":'5-px'})])), 
+             style={'height': '300px','width': '450px',"margin-left": "2px","margin-right":'5-px'})])), 
             dbc.Col(dcc.Dropdown(id='x2',
             options=[{'label': i, 'value': i} for i in ll], style={'height': '60px','font-size':25},
             multi=False,
